bf233a5c118584b214dc793f9aac44ab93bddb32
Justin Boblitt <jboblitt@vt.edu>
2012-04-29 17:49:45 -0400

    Updated help

--- Help.cpp
+++ Help.cpp
@@ -5,6 +5,8 @@ string getHelp(MODE mode, SUB_MODE submode)
     string help = "";
     switch (mode)
     {
+        case MENU:
+            help += "Menu help:";
         case SCI_CALC:
             help += "Scientific Calculator help:";
             break;
@@ -12,13 +14,20 @@ string getHelp(MODE mode, SUB_MODE submode)
             switch (submode)
             {
                 case RESISTANCE:
+                    help += "Equivalent Resistance help:";
                     break;
                 case CAPACITANCE:
+                    help += "Equivalent Capacitance help:";
                     break;
                 case INDUCTANCE:
+                    help += "Equivalent Inductance help:";
                     break;  
+                case NONE:
+                    help += "Equivalent Component help:";
+                    break;
             }
             break;
-            
     }
+    
+    return help;
 }
